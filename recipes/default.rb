1523a021b0a8b5ded49fc89ca9f7de355eda1a72
Robert Veznaver <r.veznaver@criteo.com>
2015-03-10 16:45:08 +0100

    redefine upstart service restart

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,11 +8,11 @@ This recipe call config recipe and setup a chef-rundeck service that host all br
 #>
 =end
 
-
 include_recipe 'rundeck-bridge::config'
 
 # Start chef-rundeck service
 service 'chef-rundeck' do
-  provider Chef::Provider::Service::Upstart
-  action [:enable, :start]
+  provider        Chef::Provider::Service::Upstart
+  restart_command '/sbin/stop chef-rundeck && /sbin/start chef-rundeck'
+  action          [:enable, :start]
 end
