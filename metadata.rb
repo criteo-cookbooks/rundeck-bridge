aaa6d7ed6cb2fe13049e31d3ed509a46849b3368
Xavier Krantz <x.krantz@criteo.com>
2016-05-11 16:30:02 +0200

    Update cookbook skeleton files

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name             'rundeck-bridge'
 maintainer       'Robert Veznaver'
 maintainer_email 'r.veznaver@criteo.com'
+issues_url       'https://github.com/criteo-cookbooks/rundeck-bridge/issues'
+source_url       'https://github.com/criteo-cookbooks/rundeck-bridge'
 license          'Apache License 2.0'
 description      'Installs chef-rundeck bridge and configure as needed'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
