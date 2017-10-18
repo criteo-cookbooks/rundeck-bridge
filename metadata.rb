4e109691d84be75abf8447f9693e6051c799b5b1
Grégoire Seux <g.seux@criteo.com>
2017-10-18 09:30:19 +0200

    Correct compatibility with old cehf version
    Change-Id: I3232607aa677b5013b0909b136da1470f01243a1

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name             'rundeck-bridge'
 maintainer       'Robert Veznaver'
 maintainer_email 'r.veznaver@criteo.com'
-issues_url       'https://github.com/criteo-cookbooks/rundeck-bridge/issues'
-source_url       'https://github.com/criteo-cookbooks/rundeck-bridge'
+issues_url       'https://github.com/criteo-cookbooks/rundeck-bridge/issues' if defined?(source_url)
+source_url       'https://github.com/criteo-cookbooks/rundeck-bridge' if defined?(issues_url)
 license          'Apache License 2.0'
 description      'Installs chef-rundeck bridge and configure as needed'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.1.1'
+version          '2.1.2'
 supports         'centos'
 
 depends          'chef-client'
